1ded373fbd19653152824cafb2d626969d605fd3
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-06-13 18:21:36 -0400

    setup update

---
+++ app/mjs/libs/setup.mjs
@@ -0,0 +1,41 @@
+import {black, white} from "./colors.mjs"
+
+import {addLights} from "./lights.mjs"
+import {addCamera} from "./camera.mjs"
+import {addControls} from "./controls.mjs"
+import {addEvents} from "./events.mjs"
+
+export function setup(width, height) {
+
+  data.width = width
+  data.height = height
+
+  let renderer = new THREE.WebGLRenderer()
+  renderer.setSize(width, height)
+  data.renderer = renderer
+
+  let canvas = document.body.appendChild(renderer.domElement)
+  data.canvas = canvas
+
+  let scene = new THREE.Scene()
+  scene.background = white
+  data.scene = scene
+
+  addLights()
+  addCamera()
+  addControls()
+  addEvents()
+
+  function animate() {
+
+    requestAnimationFrame(animate)
+
+    data.controls.update()
+
+    data.renderer.render(data.scene, data.camera)
+
+  }
+
+  animate()
+
+}
