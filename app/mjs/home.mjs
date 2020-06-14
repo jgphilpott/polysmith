4ce48b241f6e3c52016d1af332b8f04c3450526b
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-06-14 09:01:14 -0400

    home update

--- app/mjs/home.mjs
+++ app/mjs/home.mjs
@@ -1,52 +1,24 @@
-$(document).ready(function() {
+import {setup} from "./libs/setup.mjs"
+import {black, white} from "./libs/colors.mjs"
 
-  let black = new THREE.Color("rgba(0, 0, 0)")
-  let white = new THREE.Color("rgba(255, 255, 255)")
+$(document).ready(function() {
 
   let width = $(window).width()
   let height = $(window).height()
 
-  let renderer = new THREE.WebGLRenderer()
-  renderer.setSize(width, height)
+  setup(width, height)
 
-  let canvas = document.body.appendChild(renderer.domElement)
+  //|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|//
+  //  Paste Generator Function Below  |//
+  //|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|/|//
+  //  ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓  |//
 
-  let scene = new THREE.Scene()
-  scene.background = white
+  function generator() {
 
-  let light = new THREE.PointLight(white, 1)
-  light.position.set(10, 10, 10)
-  scene.add(light)
-
-  let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
-  camera.position.set(10, 10, 10)
-
-  let controls = new THREE.OrbitControls(camera, canvas)
-
-  $("body").keypress(function(event) {
-
-    if (event.ctrlKey && event.keyCode == 10) {
-
-      let exporter = new THREE.STLExporter()
-
-      let blob = new Blob([exporter.parse(scene)], {"type": "text/plain"})
-
-      saveAs(blob, "shape.stl")
-
-    }
-
-  })
-
-  function animate() {
-
-    requestAnimationFrame(animate)
-
-    controls.update()
-
-    renderer.render(scene, camera)
+    // Code Here!
 
   }
 
-  animate()
+  generator()
 
 })
