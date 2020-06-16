aa1dbe0a470ec16b4214040edbb818d7ce0f66d5
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-06-15 20:55:25 -0400

    basic metaCube generator

--- app/things/etc/metaCube/generator.js
+++ app/things/etc/metaCube/generator.js
@@ -1,9 +1,27 @@
-function generator() {
+function generator(count=3) {
 
-  let geometry = new THREE.BoxGeometry(50, 50, 50)
-  let material = new THREE.MeshStandardMaterial({"color": black})
-  let cube = new THREE.Mesh(geometry, material)
+  let size = 10
 
-  data.scene.add(cube)
+  let standardMaterial = new THREE.MeshStandardMaterial({"color": black})
+
+  for (let i = 0; i < count; i++) {
+
+    let boxGeometry = new THREE.BoxGeometry(size, size, size)
+    let cylinderGeometry = new THREE.CylinderGeometry(size / 3.33, size / 3.33, size * 2, 42)
+
+    let box = new THREE.Mesh(boxGeometry, standardMaterial)
+    let cylinder = new THREE.Mesh(cylinderGeometry, standardMaterial)
+
+    box = cutMesh(box, cylinder)
+    cylinder.rotation.x = Math.PI / 2
+    box = cutMesh(box, cylinder)
+    cylinder.rotation.z = Math.PI / 2
+    box = cutMesh(box, cylinder)
+
+    data.scene.add(box)
+
+    size *= 2
+
+  }
 
 }
