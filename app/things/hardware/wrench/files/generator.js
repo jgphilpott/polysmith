9216b8ea6ea7b04964ad5a71d9a19a576c17cbaf
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-04 12:19:29 -0400

    wrench update

--- app/things/hardware/wrench/files/generator.js
+++ app/things/hardware/wrench/files/generator.js
@@ -1,5 +1,5 @@
 function generator() {
 
-  importSTL("./app/things/hardware/wrench/files/Printable_Wrench.A.15.stl")
+  importSTL("./app/things/hardware/wrench/files/Printable_Wrench_w_more_gap.A.0.stl")
 
 }
