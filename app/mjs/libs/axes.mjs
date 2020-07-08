24b77672c495ea825e68382e9ded5cae69b85719
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-07 20:14:23 -0400

    axes update

--- app/mjs/libs/axes.mjs
+++ app/mjs/libs/axes.mjs
@@ -1,13 +1,31 @@
 import {addLine} from "./lines.mjs"
-import {black, white, red, green, blue} from "./colors.mjs"
+import {black, white, red, yellow, green, blue} from "./colors.mjs"
 
 export function addAxes(size=100) {
 
   let min = -size
   let max = size
 
+  addAxisX(min, max)
+  addAxisY(min, max)
+  addAxisZ(min, max)
+
+}
+
+export function addAxisX(min, max) {
+
   addLine([[max, 0, 0], [min, 0, 0]], red)
+
+}
+
+export function addAxisY(min, max) {
+
   addLine([[0, max, 0], [0, min, 0]], green)
+
+}
+
+export function addAxisZ(min, max) {
+
   addLine([[0, 0, max], [0, 0, min]], blue)
 
 }
