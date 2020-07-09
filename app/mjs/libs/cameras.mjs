431babc7d00a70a8b64b2fc7f642582d2aa657b0
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-09 03:31:19 -0400

    camera update

--- app/mjs/libs/cameras.mjs
+++ app/mjs/libs/cameras.mjs
@@ -1,8 +1,11 @@
 export function addCamera(position=data.scale) {
 
   let camera = new THREE.PerspectiveCamera(75, data.width / data.height, 0.1, 1000)
+
   camera.position.set(position, position, position)
   camera.up.set(0, 0, 1)
   data.camera = camera
+
+  return camera
 
 }
