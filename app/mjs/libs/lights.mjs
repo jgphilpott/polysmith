d65df1a382af2c65d957932e709475f222d92b7c
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-09 03:32:07 -0400

    lights update

--- app/mjs/libs/lights.mjs
+++ app/mjs/libs/lights.mjs
@@ -3,13 +3,17 @@ import {black, white, red, yellow, green, blue} from "./colors.mjs"
 export function addLights(position=data.scale) {
 
   let ambientLight = new THREE.AmbientLight(white, 1)
+
   ambientLight.position.set(0, 0, 0)
   data.scene.add(ambientLight)
 
   let pointLight = new THREE.PointLight(white, 3)
+
   pointLight.position.set(position, position, position)
   data.scene.add(pointLight)
 
   data.lights = [ambientLight, pointLight]
+
+  return data.lights
 
 }
