fd7d2269a8bdfb1b2bed5b4b4a85e0ee34fcda12
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-26 08:48:34 -0400

    text bevel update

--- app/mjs/libs/geometries/text.mjs
+++ app/mjs/libs/geometries/text.mjs
@@ -2,7 +2,7 @@
 
 import {standardMaterial} from "../materials.mjs"
 
-export function newText(text, size=12, font="ubuntu") {
+export function newText(text, size=12, bevel=false, font="ubuntu") {
 
   let textGeometry = new THREE.TextGeometry(text, {
 
@@ -12,7 +12,7 @@ export function newText(text, size=12, font="ubuntu") {
     "height": size / 10,
     "curveSegments": 12,
 
-    "bevelEnabled": true,
+    "bevelEnabled": bevel,
     "bevelThickness": 1,
     "bevelSize": 1,
     "bevelOffset": 0,
@@ -24,13 +24,13 @@ export function newText(text, size=12, font="ubuntu") {
 
 }
 
-export function addText(text, size=12, font="ubuntu") {
+export function addText(text, size=12, bevel=false, font="ubuntu") {
 
   let loader = new THREE.FontLoader()
 
   loader.load("./app/fonts/" + font + ".json", function (font) {
 
-    let textGeometry = newText(text, size, font)
+    let textGeometry = newText(text, size, bevel, font)
 
     let mesh = new THREE.Mesh(textGeometry, standardMaterial())
 
