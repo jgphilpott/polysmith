5d6457a11120b2016fc72fdad11a243240b459eb
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-26 09:53:36 -0400

    cylinders update

--- app/mjs/libs/geometries/cylinders.mjs
+++ app/mjs/libs/geometries/cylinders.mjs
@@ -1,9 +1,26 @@
 // Link: https://threejs.org/docs/#api/en/geometries/CylinderGeometry
 
-export function newCylinder() {
+import {black, white, red, yellow, green, blue} from "../colors.mjs"
+
+import {standardMaterial} from "../materials.mjs"
+
+export function newCylinder(length=10, positiveRadius=10, negativeRadius=10, position=[0, 0, 0], color=black, radialSegments=42) {
+
+  let geometry = new THREE.CylinderGeometry(positiveRadius, negativeRadius, length, radialSegments)
+  let cylinder = new THREE.Mesh(geometry, standardMaterial(color))
+
+  cylinder.position.set(position[0], position[1], position[2])
+
+  return cylinder
 
 }
 
-export function addCylinder() {
+export function addCylinder(length=10, positiveRadius=10, negativeRadius=10, position=[0, 0, 0], color=black, radialSegments=42) {
+
+  let cylinder = newCylinder(length, positiveRadius, negativeRadius, position, color, radialSegments)
+
+  data.scene.add(cylinder)
+
+  return cylinder
 
 }
