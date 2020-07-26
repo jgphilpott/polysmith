b700f5b9230ad2fc5de244194969dfd24bef7716
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-26 10:03:04 -0400

    boxes update

--- app/mjs/libs/geometries/boxes.mjs
+++ app/mjs/libs/geometries/boxes.mjs
@@ -1,9 +1,26 @@
 // Link: https://threejs.org/docs/#api/en/geometries/BoxGeometry
 
-export function newBox() {
+import {black, white, red, yellow, green, blue} from "../colors.mjs"
+
+import {standardMaterial} from "../materials.mjs"
+
+export function newBox(width=10, height=10, depth=10, position=[0, 0, 0], color=black) {
+
+  let geometry = new THREE.BoxGeometry(width, height, depth)
+  let box = new THREE.Mesh(geometry, standardMaterial(color))
+
+  box.position.set(position[0], position[1], position[2])
+
+  return box
 
 }
 
-export function addBox() {
+export function addBox(width=10, height=10, depth=10, position=[0, 0, 0], color=black) {
+
+  let box = newBox(width, height, depth, position, color)
+
+  data.scene.add(box)
+
+  return box
 
 }
