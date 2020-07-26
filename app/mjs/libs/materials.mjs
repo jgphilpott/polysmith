7f56b71a8a5de140703336690c04fb279939c741
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-26 08:35:49 -0400

    materials update

--- app/mjs/libs/materials.mjs
+++ app/mjs/libs/materials.mjs
@@ -1,3 +1,9 @@
 import {black, white, red, orange, yellow, green, blue} from "./colors.mjs"
 
 export let standardMaterial = new THREE.MeshStandardMaterial({"color": black})
+
+export function lineMaterial(color=black, linewidth=1) {
+
+  return new THREE.LineBasicMaterial({"color": color, "linewidth": linewidth})
+
+}
