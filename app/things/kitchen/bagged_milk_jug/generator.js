47aef0abbbcabf6bb493109e4d265a00d1c043c9
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-26 14:28:58 -0400

    bagged_milk_jug in progress

--- app/things/kitchen/bagged_milk_jug/generator.js
+++ app/things/kitchen/bagged_milk_jug/generator.js
@@ -1,1 +1,23 @@
-function generator() {}
+function generator(lang="en") {
+
+  let offset = 15
+  let thickness = 2
+
+  let height = 200
+
+  let topRadius = 45
+  let bottomRadius = 40
+  let radialSegments = 101
+
+  let frontInteriorCylinder = newCylinder(height, topRadius, bottomRadius, [offset, thickness, 0], black, radialSegments).rotateX(Math.PI / 2)
+  let frontExteriorCylinder = newCylinder(height, topRadius + thickness, bottomRadius + thickness, [offset, 0, 0], black, radialSegments).rotateX(Math.PI / 2)
+  let backInteriorCylinder = newCylinder(height, topRadius, bottomRadius, [-offset, thickness, 0], black, radialSegments).rotateX(Math.PI / 2)
+  let backExteriorCylinder = newCylinder(height, topRadius + thickness, bottomRadius + thickness, [-offset, 0, 0], black, radialSegments).rotateX(Math.PI / 2)
+
+  let frontCylinder = cutMesh(frontExteriorCylinder, frontInteriorCylinder)
+  let backCylinder = cutMesh(backExteriorCylinder, backInteriorCylinder)
+
+  data.scene.add(frontCylinder)
+  data.scene.add(backCylinder)
+
+}
