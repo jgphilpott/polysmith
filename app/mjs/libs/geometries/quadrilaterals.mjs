ce9120d671e3179cfd070cd822b73332d62164ac
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-07-29 19:57:36 -0400

    adding quadrilaterals

---
+++ app/mjs/libs/geometries/quadrilaterals.mjs
@@ -0,0 +1,31 @@
+import {black, white, red, yellow, green, blue} from "../colors.mjs"
+
+import {standardMaterial} from "../materials.mjs"
+
+export function newQuadrilateral(v1=[0, 0, 10], v2=[0, 10, 10], v3=[0, 10, 0], v4=[0, 0, 0], color=black) {
+
+  let geometry = new THREE.Geometry()
+
+  geometry.vertices.push(new THREE.Vector3(v1[0], v1[1], v1[2]))
+  geometry.vertices.push(new THREE.Vector3(v2[0], v2[1], v2[2]))
+  geometry.vertices.push(new THREE.Vector3(v3[0], v3[1], v3[2]))
+  geometry.vertices.push(new THREE.Vector3(v4[0], v4[1], v4[2]))
+
+  geometry.faces.push(new THREE.Face3(0, 1, 2))
+  geometry.faces.push(new THREE.Face3(0, 3, 2))
+
+  let quadrilateral = new THREE.Mesh(geometry, standardMaterial(color))
+
+  return quadrilateral
+
+}
+
+export function addQuadrilateral(v1=[0, 0, 10], v2=[0, 10, 10], v3=[0, 10, 0], v4=[0, 0, 0], color=black) {
+
+  let quadrilateral = newQuadrilateral(v1, v2, v3, v4, color)
+
+  data.scene.add(quadrilateral)
+
+  return quadrilateral
+
+}
