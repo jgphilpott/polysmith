53d30086a09d29ec14de0cb8cfc26b9740f5d673
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-08-15 10:57:53 -0400

    adding 5V_step_motor generator

--- app/things/hardware/motors/5V_step_motor/generator.js
+++ app/things/hardware/motors/5V_step_motor/generator.js
@@ -1,3 +1,33 @@
 function generator(collar=false) {
 
+  let shaftLength = 8
+  let shaftRadius = 2.5
+
+  let cutLength = 6
+  let cutDepth = 1
+
+  let collarLength = 1.5
+  let collarRadius = 5
+
+  let shaft = newCylinder(shaftLength, shaftRadius, shaftRadius)
+
+  let cutOne = newBox(shaftLength, shaftLength, shaftLength, [0, shaftLength - cutLength, (shaftLength / 2 + shaftRadius) - cutDepth])
+  let cutTwo = newBox(shaftLength, shaftLength, shaftLength, [0, shaftLength - cutLength, - ((shaftLength / 2 + shaftRadius) - cutDepth)])
+
+  shaft = cutMesh(cutMesh(shaft, cutOne), cutTwo).rotateX(tools.degree2radian(90))
+
+  if (collar) {
+
+    addCylinder(collarLength, collarRadius, collarRadius, [0, 0, collarLength / 2]).rotateX(tools.degree2radian(90))
+
+    shaft.position.set(0, 0, (shaftLength / 2) + collarLength)
+    data.scene.add(shaft)
+
+  } else {
+
+    shaft.position.set(0, 0, shaftLength / 2)
+    data.scene.add(shaft)
+
+  }
+
 }
