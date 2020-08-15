4b06438f693e58fe65f1a44bddeac4284dc37e56
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-08-14 23:24:36 -0400

    SM24240 motor generator update

--- app/things/hardware/motors/SM24240/generator.js
+++ app/things/hardware/motors/SM24240/generator.js
@@ -1,3 +1,32 @@
 function generator(collar=false) {
 
+  let shaftLength = 20
+  let shaftRadius = 2.5
+
+  let cutLength = 18
+  let cutDepth = 0.8
+
+  let collarLength = 2
+  let collarRadius = 11
+
+  let shaft = newCylinder(shaftLength, shaftRadius, shaftRadius)
+  let cut = newBox(shaftLength, shaftLength, shaftLength, [0, shaftLength - cutLength, (shaftLength / 2 + shaftRadius) - cutDepth])
+
+  shaft = cutMesh(shaft, cut).rotateX(tools.degree2radian(90))
+
+  if (collar) {
+
+    collar = newCylinder(collarLength, collarRadius, collarRadius, [0, 0, collarLength / 2]).rotateX(tools.degree2radian(90))
+    shaft.position.set(0, 0, (shaftLength / 2) + collarLength)
+
+    data.scene.add(collar)
+    data.scene.add(shaft)
+
+  } else {
+
+    shaft.position.set(0, 0, shaftLength / 2)
+    data.scene.add(shaft)
+
+  }
+
 }
