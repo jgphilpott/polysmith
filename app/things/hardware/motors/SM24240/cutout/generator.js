5bf6490826d0e5e69af479b7ea5cd25e8b36fb4e
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-08-15 01:00:08 -0400

    adding SM24240 motor cutout generator

---
+++ app/things/hardware/motors/SM24240/cutout/generator.js
@@ -0,0 +1,36 @@
+function generator(collar=false) {
+
+  let cutoutSize = 25
+
+  let shaftLength = 20
+  let shaftRadius = 2.5
+
+  let cutLength = 18
+  let cutDepth = 0.8
+
+  let collarLength = 2
+  let collarRadius = 11
+
+  let shaft = newCylinder(shaftLength, shaftRadius, shaftRadius)
+  let cut = newBox(shaftLength, shaftLength, shaftLength, [0, shaftLength - cutLength, (shaftLength / 2 + shaftRadius) - cutDepth])
+
+  shaft = cutMesh(shaft, cut).rotateX(tools.degree2radian(-90))
+
+  let cutout = newBox(cutoutSize / 2, cutoutSize * 2, cutoutSize, [0, cutoutSize / 2, -(cutoutSize / 2)])
+
+  if (collar) {
+
+    collar = newCylinder(collarLength, collarRadius, collarRadius, [0, collarLength / 2, 0]).rotateX(tools.degree2radian(-90))
+    shaft.position.set(0, (shaftLength / 2) + collarLength, 0)
+    cutout = cutMesh(cutMesh(cutout, shaft), collar)
+    data.scene.add(cutout)
+
+  } else {
+
+    shaft.position.set(0, shaftLength / 2, 0)
+    cutout = cutMesh(cutout, shaft)
+    data.scene.add(cutout)
+
+  }
+
+}
