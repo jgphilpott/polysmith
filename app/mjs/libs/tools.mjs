07331541364ba62dd7f8b8d3d90d0b2ab1b9cc8a
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-08-19 19:03:18 -0400

    adding BoundingBox tool

--- app/mjs/libs/tools.mjs
+++ app/mjs/libs/tools.mjs
@@ -9,3 +9,34 @@ export function radian2degree(radian) {
   return radian * (180 / Math.PI)
 
 }
+
+export function getBoundingBox(mesh) {
+
+  if (!mesh.geometry.boundingBox) {
+
+    let xVertices = []
+    let yVertices = []
+    let zVertices = []
+
+    for (let i = 0; i < mesh.geometry.vertices.length; i++) {
+
+      xVertices.push(mesh.geometry.vertices[i].x)
+      yVertices.push(mesh.geometry.vertices[i].y)
+      zVertices.push(mesh.geometry.vertices[i].z)
+
+    }
+
+    return {
+
+      "min": {"x": minValue(xVertices), "y": minValue(yVertices), "z": minValue(zVertices)},
+      "max": {"x": maxValue(xVertices), "y": maxValue(yVertices), "z": maxValue(zVertices)}
+
+    }
+
+  } else {
+
+    return mesh.geometry.boundingBox
+
+  }
+
+}
