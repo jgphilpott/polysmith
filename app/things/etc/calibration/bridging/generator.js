03ac78661cceaab99952e3ccb1f49c701749c8d7
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-09-26 15:44:46 -0400

    adding bridging generator to calibrator

--- app/things/etc/calibration/bridging/generator.js
+++ app/things/etc/calibration/bridging/generator.js
@@ -1,3 +1,25 @@
-function generator() {
+function generator(bridgeLength=1) {
+
+  let segments = bridgeLength * 2
+  let segmentWidth = 5
+
+  let baseWidth = ((segments * 2) - 1) * segmentWidth
+  let baseLength = (bridgeLength * 10) + (segmentWidth * 2)
+  let base = newBox(baseLength + segmentWidth, baseWidth + segmentWidth, segmentWidth, [baseLength / 2, baseWidth / 2, - (segmentWidth / 2)])
+
+  for (let i = 0; i < segments; i++) {
+
+    let pillar0 = newBox(segmentWidth, segmentWidth, segmentWidth * 3, [segmentWidth / 2, (segmentWidth / 2) + (i * 10), segmentWidth / 2])
+    let pillar1 = newBox(segmentWidth, segmentWidth, segmentWidth * 3, [(segmentWidth / 2) + (i * segmentWidth) + (segmentWidth * 2), (segmentWidth / 2) + (i * 10), segmentWidth / 2])
+
+    let bridgeLength = (segmentWidth * (i + 1)) + (segmentWidth * 2)
+    let bridge = newBox(bridgeLength, segmentWidth, segmentWidth, [bridgeLength / 2, (segmentWidth / 2) + (i * 10), segmentWidth * 1.5])
+    bridge = joinMesh(joinMesh(bridge, pillar0), pillar1)
+
+    base = joinMesh(base, bridge)
+
+  }
+
+  data.scene.add(base)
 
 }
