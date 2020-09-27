21c6db0922e8212a5de2d691846ea60411801cc9
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-09-27 12:34:39 -0400

    adding overhang test generator

--- app/things/etc/calibration/overhang/generator.js
+++ app/things/etc/calibration/overhang/generator.js
@@ -1,3 +1,20 @@
-function generator() {
+function generator(degree=10, size=50) {
+
+  let steps = (90 / degree) - 1
+
+  let thickness = 5
+
+  let base = newBox(size, size, thickness, [ - (size / 2), 0, - (thickness / 2)])
+
+  let archExterior = newCylinder(size, size, size, [0, 0, 0], black, (steps + 1) * 4).rotateY(tools.degree2radian(45 / (steps + 1)))
+  let archInterior = newCylinder(size + 1, size - thickness, size - thickness, [0, 0, 0], black, (steps + 1) * 4).rotateY(tools.degree2radian(45 / (steps + 1)))
+
+  let archXcleanup = newBox(size * 4, size * 4, size * 4, [size * 2, 0, 0])
+  let archZcleanup = newBox(size * 4, size * 4, size * 4, [0, 0, - (size * 2)])
+
+  let arch = cutMesh(archExterior, archInterior)
+  arch = cutMesh(cutMesh(arch, archXcleanup), archZcleanup)
+
+  data.scene.add(joinMesh(arch, base))
 
 }
