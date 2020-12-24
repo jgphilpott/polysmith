88158cfdde6ce2e36071d761b65448473a5404d8
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-12-24 07:18:21 -0500

    root update

--- app/root.py
+++ app/root.py
@@ -20,24 +20,24 @@ if not exists(libs_dir):
 
     makedirs(libs_dir)
 
-urlretrieve("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js", libs_dir + "/jQuery.js")
+    urlretrieve("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js", libs_dir + "/jQuery.js")
 
-urlretrieve("http://threejs.org/build/three.min.js", libs_dir + "/three.js")
+    urlretrieve("http://threejs.org/build/three.min.js", libs_dir + "/three.js")
 
-urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/23f814719b2d199e324448751862f2a5fc7d27a5/examples/js/controls/OrbitControls.js", libs_dir + "/orbitControls.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/23f814719b2d199e324448751862f2a5fc7d27a5/examples/js/controls/OrbitControls.js", libs_dir + "/orbitControls.js")
 
-urlretrieve("https://raw.githubusercontent.com/evanw/csg.js/a8512afbac3cf503195870f7ef11c0a32f36c6d4/csg.js", libs_dir + "/csg.js")
-urlretrieve("https://raw.githubusercontent.com/kraag22/csg-wrapper/1d2182b6265282ea9f1f3d66bd4c0bb2b87a232b/ThreeCSG.js", libs_dir + "/csgWrapper.js")
-urlretrieve("https://gist.githubusercontent.com/jgphilpott/59ad8432ba8567e91176e669454b9afa/raw/381ceef76f9b0e69b492910306434c6f52ba420d/meshOperations.js", libs_dir + "/meshOperations.js")
+    urlretrieve("https://raw.githubusercontent.com/evanw/csg.js/a8512afbac3cf503195870f7ef11c0a32f36c6d4/csg.js", libs_dir + "/csg.js")
+    urlretrieve("https://raw.githubusercontent.com/kraag22/csg-wrapper/1d2182b6265282ea9f1f3d66bd4c0bb2b87a232b/ThreeCSG.js", libs_dir + "/csgWrapper.js")
+    urlretrieve("https://gist.githubusercontent.com/jgphilpott/59ad8432ba8567e91176e669454b9afa/raw/381ceef76f9b0e69b492910306434c6f52ba420d/meshOperations.js", libs_dir + "/meshOperations.js")
 
-urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/loaders/STLLoader.js", libs_dir + "/STLLoader.js")
-urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/exporters/STLExporter.js", libs_dir + "/STLExporter.js")
-urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/loaders/OBJLoader.js", libs_dir + "/OBJLoader.js")
-urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/exporters/OBJExporter.js", libs_dir + "/OBJExporter.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/loaders/STLLoader.js", libs_dir + "/STLLoader.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/exporters/STLExporter.js", libs_dir + "/STLExporter.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/loaders/OBJLoader.js", libs_dir + "/OBJLoader.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/exporters/OBJExporter.js", libs_dir + "/OBJExporter.js")
 
-urlretrieve("https://raw.githubusercontent.com/eligrey/FileSaver.js/master/src/FileSaver.js", libs_dir + "/FileSaver.js")
+    urlretrieve("https://raw.githubusercontent.com/eligrey/FileSaver.js/master/src/FileSaver.js", libs_dir + "/FileSaver.js")
 
-urlretrieve("https://gist.githubusercontent.com/jgphilpott/6332dc7f5636db9ba455e1575407c496/raw/b72589532af0b7c63e321b15254acbb848248209/scaling.js", libs_dir + "/tools.js")
+    urlretrieve("https://gist.githubusercontent.com/jgphilpott/6332dc7f5636db9ba455e1575407c496/raw/b72589532af0b7c63e321b15254acbb848248209/scaling.js", libs_dir + "/tools.js")
 
 compile(dirname=("app/sass", "app/css"))
 
