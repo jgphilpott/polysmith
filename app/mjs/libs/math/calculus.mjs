f1f75ef4bf53b2cfc9d74c8c65ad4edcd6218c93
Jacob Philpott <jacob.philpott@gmx.com>
2021-01-29 18:16:56 +0300

    adding area function to calculus lib

--- app/mjs/libs/math/calculus.mjs
+++ app/mjs/libs/math/calculus.mjs
@@ -19,3 +19,19 @@ export function findTangent(x, coefficients) {
   return [intercept, slope]
 
 }
+
+export function findArea(lowerLimit, upperLimit, coefficients) {
+
+  let lower = 0
+  let upper = 0
+
+  for (let i = 0; i < coefficients.length; i++) {
+
+    lower += (coefficients[i] * Math.pow(lowerLimit, i + 1)) / (i + 1)
+    upper += (coefficients[i] * Math.pow(upperLimit, i + 1)) / (i + 1)
+
+  }
+
+  return Math.abs(upper - lower)
+
+}
