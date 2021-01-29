75d949048b396ceaed95cb12303d544a28037566
Jacob Philpott <jacob.philpott@gmx.com>
2021-01-29 19:06:51 +0300

    adding notes to calculus lib

--- app/mjs/libs/math/calculus.mjs
+++ app/mjs/libs/math/calculus.mjs
@@ -1,3 +1,4 @@
+// Find the tangent of a polynomial function at x.
 export function findTangent(x, coefficients) {
 
   let slope = 0
@@ -14,6 +15,9 @@ export function findTangent(x, coefficients) {
 
 }
 
+// Find the area under a polynomial function within bounds.
+// This function works assuming your curve is either entirely above or entirely below the x axis.
+// If your curve crosses the x axis within the given bounds you will need to first find the roots (x intercepts) and then calculate the segments above and below the axis separately.
 export function findArea(lowerLimit, upperLimit, coefficients) {
 
   let lower = 0
