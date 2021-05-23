2f2fdcc835afbb849df810b4f3936a6943333b68
Jacob Philpott <jacob.philpott@gmx.com>
2021-05-22 20:05:38 -0400

    glass generator update and ruff copy STLs

--- app/things/kitchen/glass/generator.js
+++ app/things/kitchen/glass/generator.js
@@ -1,1 +1,12 @@
-function generator() {}
+function generator(height=150, topRadius=40, bottomRadius=30, thickness=2, segments=10) {
+
+  let glassInterior = newCylinder(height, topRadius, bottomRadius, [0, 0, 0], black, segments)
+  let glassExterior = newCylinder(height, topRadius + thickness, bottomRadius + thickness, [0, -thickness, 0], black, segments)
+
+  let glass = cutMesh(glassExterior, glassInterior)
+
+  glass.rotation.x = tools.degree2radian(90)
+
+  data.scene.add(glass)
+
+}
