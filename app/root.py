de7cddc9ada4aab2fc4faecf64ddd0df8449e48c
Jacob Philpott <jacob.philpott@gmx.com>
2021-05-25 18:04:42 -0400

    three.js update

--- app/root.py
+++ app/root.py
@@ -22,17 +22,19 @@ if not exists(libs_dir):
 
     urlretrieve("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js", libs_dir + "/jQuery.js")
 
-    urlretrieve("http://threejs.org/build/three.min.js", libs_dir + "/three.js")
+    urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js", libs_dir + "/three.js")
     urlretrieve("https://raw.githubusercontent.com/jeromeetienne/threex.domevents/1100828c40df668a82a97c811895918088cc607f/threex.domevents.js", libs_dir + "/threeX.js")
 
     urlretrieve("https://raw.githubusercontent.com/evanw/csg.js/a8512afbac3cf503195870f7ef11c0a32f36c6d4/csg.js", libs_dir + "/csg.js")
     urlretrieve("https://raw.githubusercontent.com/kraag22/csg-wrapper/1d2182b6265282ea9f1f3d66bd4c0bb2b87a232b/ThreeCSG.js", libs_dir + "/csgWrapper.js")
     urlretrieve("https://gist.githubusercontent.com/jgphilpott/59ad8432ba8567e91176e669454b9afa/raw/381ceef76f9b0e69b492910306434c6f52ba420d/meshOperations.js", libs_dir + "/meshOperations.js")
 
-    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/loaders/STLLoader.js", libs_dir + "/STLLoader.js")
-    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/exporters/STLExporter.js", libs_dir + "/STLExporter.js")
-    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/loaders/OBJLoader.js", libs_dir + "/OBJLoader.js")
-    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/8880636d522afded0c481a59ef2815c11ed64bda/examples/js/exporters/OBJExporter.js", libs_dir + "/OBJExporter.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/71c8b3f6cbf8da5d1939608314002b70a3d4a2d7/examples/js/loaders/OBJLoader.js", libs_dir + "/OBJLoader.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/71c8b3f6cbf8da5d1939608314002b70a3d4a2d7/examples/js/loaders/STLLoader.js", libs_dir + "/STLLoader.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/71c8b3f6cbf8da5d1939608314002b70a3d4a2d7/examples/js/loaders/SVGLoader.js", libs_dir + "/SVGLoader.js")
+
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/71c8b3f6cbf8da5d1939608314002b70a3d4a2d7/examples/js/exporters/OBJExporter.js", libs_dir + "/OBJExporter.js")
+    urlretrieve("https://raw.githubusercontent.com/mrdoob/three.js/71c8b3f6cbf8da5d1939608314002b70a3d4a2d7/examples/js/exporters/STLExporter.js", libs_dir + "/STLExporter.js")
 
     urlretrieve("https://raw.githubusercontent.com/eligrey/FileSaver.js/b5e61ec88969461ce0504658af07c2b56650ee8c/src/FileSaver.js", libs_dir + "/FileSaver.js")
 
