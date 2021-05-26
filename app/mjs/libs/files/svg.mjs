09ac25777f725706ae1e1ba1aa8ba0043853cc29
Jacob Philpott <jacob.philpott@gmx.com>
2021-05-25 20:06:15 -0400

    svg loader update

--- app/mjs/libs/files/svg.mjs
+++ app/mjs/libs/files/svg.mjs
@@ -1,1 +1,25 @@
-export function importSVG(path) {}
+import {black, white} from "../colors.mjs"
+
+export function importSVG(path, depth=10, bevelEnabled=false, center=true) {
+
+  let shapes = []
+  let paths = new THREE.SVGLoader().parse(path).paths
+
+  let extrusionSettings = {depth: depth,
+                           bevelEnabled: bevelEnabled}
+
+  for (let i = 0; i < paths.length; i ++) {
+    Array.prototype.push.apply(shapes, paths[i].toShapes())
+  }
+
+  let geometry = new THREE.ExtrudeBufferGeometry(shapes, extrusionSettings)
+
+  if (center) {geometry.center()}
+
+  let mesh = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({color: black}))
+
+  data.meshes.push(mesh)
+
+  return mesh
+
+}
