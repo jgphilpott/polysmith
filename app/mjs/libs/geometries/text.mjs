f1701a685815383b32b53d4982bcdcfd65bf77cc
Jacob Philpott <jacob.philpott@gmx.com>
2021-05-29 15:41:30 -0400

    Text Geometry Update

--- app/mjs/libs/geometries/text.mjs
+++ app/mjs/libs/geometries/text.mjs
@@ -1,8 +1,10 @@
 // Link: https://threejs.org/docs/#api/en/geometries/TextGeometry
 
+import * as tools from "../tools.mjs"
+
 import {standardMaterial} from "../materials.mjs"
 
-export function newText(text, size=12, height=2, bevel=false, font="arial") {
+export function newText(text, size=12, height=2, bevel=false, font="ubuntu", center=true, x=0, y=0, z=0, rotateX=0, rotateY=0, rotateZ=0) {
 
   let textGeometry = new THREE.TextGeometry(text, {
 
@@ -20,19 +22,27 @@ export function newText(text, size=12, height=2, bevel=false, font="arial") {
 
   })
 
+  if (center) {textGeometry.center()}
+
   let textMesh = new THREE.Mesh(textGeometry, standardMaterial())
 
   return textMesh
 
 }
 
-export function addText(text, size=12, height=2, bevel=false, font="arial") {
+export function addText(text, size=12, height=2, bevel=false, font="ubuntu", center=true, x=0, y=0, z=0, rotateX=0, rotateY=0, rotateZ=0) {
 
   let loader = new THREE.FontLoader()
 
   loader.load("./app/fonts/" + font + ".json", function (font) {
 
-    let textMesh = newText(text, size, height, bevel, font)
+    let textMesh = newText(text, size, height, bevel, font, center, x, y, z, rotateX, rotateY, rotateZ)
+
+    textMesh.rotateX(tools.degree2radian(rotateX))
+    textMesh.rotateY(tools.degree2radian(rotateY))
+    textMesh.rotateZ(tools.degree2radian(rotateZ))
+
+    textMesh.position.set(x, y, z)
 
     data.meshes.push(textMesh)
     data.scene.add(textMesh)
