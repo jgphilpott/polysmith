02d86e714092529850eaa78db5f899e36144bbf1
Jacob Philpott <jacob.philpott@gmx.com>
2021-06-09 20:58:37 -0400

    starting golf cup

--- app/things/sports/golf/cup/generator.js
+++ app/things/sports/golf/cup/generator.js
@@ -1,1 +1,14 @@
-function generator() {}
+function generator() {
+
+  let radius = 54
+  let depth = 152.4
+  let thickness = 2.54
+
+  let cupExterior = newCylinder(depth, radius, radius)
+  let cupInterior = newCylinder(depth, radius - thickness, radius - thickness)
+
+  let cup = cutMesh(cupExterior, cupInterior)
+
+  data.scene.add(cup)
+
+}
