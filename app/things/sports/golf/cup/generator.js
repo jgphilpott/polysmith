8a1c0156e673b99124014d0cd7b2fb53c17e54e1
Jacob Philpott <jacob.philpott@gmx.com>
2021-06-12 16:29:25 -0400

    golf cup generator update

--- app/things/sports/golf/cup/generator.js
+++ app/things/sports/golf/cup/generator.js
@@ -18,8 +18,28 @@ function generator() {
   let flagstickExterior = newCylinder(radius / 2, flagstick + thickness, flagstick + thickness, [0, - (depth / 2) + (depth / 3) - (radius / 4), 0], black, segments)
   let flagstickInterior = newCylinder(radius / 2, flagstick, flagstick, [0, - (depth / 2) + (depth / 3) - (radius / 4), 0], black, segments)
 
+  let holeRadius = (radius - thickness - flagstick) / 2
+  let holeCutter = newBox(radius * 2, depth, radius * 2, [0, 0, - radius])
+  let holeExteriorRadius = cutMesh(cupExterior, newCylinder(depth, flagstick + (holeRadius * 1.5), flagstick + (holeRadius * 1.5), [0, 0, 0], black, segments))
+  let holeInteriorRadius = newCylinder(depth, flagstick + (holeRadius / 2), flagstick + (holeRadius / 2), [0, 0, 0], black, segments)
+
+  holeCutter.rotation.y = tools.degree2radian(-30)
+  let holeOne = cutMesh(cupExterior, holeCutter)
+  holeCutter.rotation.y = tools.degree2radian(30)
+  holeOne = cutMesh(cutMesh(intersectMesh(holeOne, holeCutter), holeExteriorRadius), holeInteriorRadius)
+
+  holeCutter.rotation.y = tools.degree2radian(-150)
+  let holeTwo = cutMesh(cupExterior, holeCutter)
+  holeCutter.rotation.y = tools.degree2radian(-90)
+  holeTwo = cutMesh(cutMesh(intersectMesh(holeTwo, holeCutter), holeExteriorRadius), holeInteriorRadius)
+
+  holeCutter.rotation.y = tools.degree2radian(150)
+  let holeThree = intersectMesh(cupExterior, holeCutter)
+  holeCutter.rotation.y = tools.degree2radian(90)
+  holeThree = cutMesh(cutMesh(cutMesh(holeThree, holeCutter), holeExteriorRadius), holeInteriorRadius)
+
   cup = cutMesh(cupExterior, cupInterior)
-  base = cutMesh(cutMesh(joinMesh(base, baseBottom), baseTop), flagstickInterior)
+  base = cutMesh(cutMesh(cutMesh(cutMesh(cutMesh(joinMesh(base, baseBottom), baseTop), flagstickInterior), holeOne), holeTwo), holeThree)
   flagstick = cutMesh(cutMesh(flagstickExterior, flagstickInterior), baseTop)
 
   data.scene.add(cup)
