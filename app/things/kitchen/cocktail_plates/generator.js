302d44a2d742f6f3e1628e3505f59e417eb55e7b
Jacob Philpott <jacob.philpott@gmx.com>
2021-09-05 09:06:17 -0400

    cocktail plates generator update

--- app/things/kitchen/cocktail_plates/generator.js
+++ app/things/kitchen/cocktail_plates/generator.js
@@ -1,1 +1,13 @@
-function generator(sides=3) {}
+function generator(sides=3, size=150) {
+
+  let thickness = 2
+  let lipSize = 10
+
+  let plate = newCylinder(thickness + lipSize, size + lipSize, size, [0, 0, 0], black, sides).rotateX(tools.degree2radian(90))
+  let lip = newCylinder(thickness + lipSize, size + lipSize - thickness, size - thickness, [0, thickness, 0], black, sides).rotateX(tools.degree2radian(90))
+
+  let cocktail_plate = cutMesh(plate, lip)
+
+  data.scene.add(cocktail_plate)
+
+}
