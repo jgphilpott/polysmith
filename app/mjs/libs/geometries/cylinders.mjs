4223bf4c2a198f7c3df527a92b47a19169bf8aa3
Jacob Philpott <jacob.philpott@gmx.com>
2021-11-24 14:54:26 -0500

    cylinder geometry update

--- app/mjs/libs/geometries/cylinders.mjs
+++ app/mjs/libs/geometries/cylinders.mjs
@@ -3,7 +3,7 @@
 import {black} from "../colors/grayscale.mjs"
 import {meshMaterial} from "../materials/mesh.mjs"
 
-export function newCylinder(length=10, positiveRadius=5, negativeRadius=5, position=[0, 0, 0], material="normal", color=black, radialSegments=42) {
+export function newCylinder(length=10, positiveRadius=5, negativeRadius=5, radialSegments=42, position=[0, 0, 0], material="normal", color=black) {
 
   let geometry = new THREE.CylinderGeometry(positiveRadius, negativeRadius, length, radialSegments)
   let cylinder = new THREE.Mesh(geometry, meshMaterial(material, color))
@@ -14,9 +14,9 @@ export function newCylinder(length=10, positiveRadius=5, negativeRadius=5, posit
 
 }
 
-export function addCylinder(length=10, positiveRadius=5, negativeRadius=5, position=[0, 0, 0], material="normal", color=black, radialSegments=42) {
+export function addCylinder(length=10, positiveRadius=5, negativeRadius=5, radialSegments=42, position=[0, 0, 0], material="normal", color=black) {
 
-  let cylinder = newCylinder(length, positiveRadius, negativeRadius, position, material, color, radialSegments)
+  let cylinder = newCylinder(length, positiveRadius, negativeRadius, radialSegments, position, material, color)
 
   data.scene.add(cylinder)
 
