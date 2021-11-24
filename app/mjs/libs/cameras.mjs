f48ea55d8edbcdcb30cc8ed4bfb77c738254d6bc
Jacob Philpott <jacob.philpott@gmx.com>
2021-11-23 19:48:56 -0500

    updates

--- app/mjs/libs/cameras.mjs
+++ app/mjs/libs/cameras.mjs
@@ -1,9 +1,9 @@
-export function addCamera(position=115) {
+export function addCamera() {
 
   let camera = new THREE.PerspectiveCamera(75, data.width / data.height, 0.1, 1000)
 
   camera.up.set(0, 0, 1)
-  camera.position.set(position, position, position)
+  camera.position.set(135, 135, 55)
 
   camera.target = {x: 0, y: 0, z: 0}
   camera.lookAt(camera.target.x, camera.target.y, camera.target.z)
---
+++ app/mjs/libs/geometries/README.md
@@ -0,0 +1,2 @@
+ - Read this three.js doc to learn more about [geometries](https://threejs.org/docs/#api/en/core/BufferGeometry).
+ - Checkout the three.js source code on GitHub to see [all the geometries available](https://github.com/mrdoob/three.js/tree/670b1e9e85356d98efa4c702e93c85dd52f01e1e/src/geometries).
