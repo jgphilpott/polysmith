c3b5aaedc8a73e076686b7d0647279ac6686e230
Jacob Philpott <jacob.philpott@gmx.com>
2022-02-01 18:54:34 +0300

    shapes panel update

--- app/mjs/panels/shapes.mjs
+++ app/mjs/panels/shapes.mjs
@@ -4,4 +4,10 @@ export function addShapesPanel() {
 
   let panel = $("#shapes.panel")
 
+  panel.append("<img id='cube' class='shape' src='/app/imgs/icons/shapes/basic/cube.png'>")
+  panel.append("<img id='cylinder' class='shape' src='/app/imgs/icons/shapes/basic/cylinder.png'>")
+  panel.append("<img id='sphere' class='shape' src='/app/imgs/icons/shapes/basic/sphere.png'>")
+
+  panel.append("<img id='+' class='nav' src='/app/imgs/icons/nav/+.png'>")
+
 }
