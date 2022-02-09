c861924616d8516c48473fcf699ab27d26a211ef
Jacob <jgp14@queensu.ca>
2022-02-09 15:06:53 +0300

    docker-compose update

--- app/root.py
+++ app/root.py
@@ -58,4 +58,4 @@ if not exists(libs_dir):
 compile(dirname=("app/sass", "app/css"), output_style="compressed")
 Popen(["boussole", "watch"], cwd="app/config")
 
-app.run(host="0.0.0.0", port=4000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
