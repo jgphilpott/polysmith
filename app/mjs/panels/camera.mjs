c8138b6675e6437fbfbe44cbfeb5d0ce37945e70
Jacob Philpott <jacob.philpott@gmx.com>
2022-02-27 02:09:07 +0300

    camera panel update

--- app/mjs/panels/camera.mjs
+++ app/mjs/panels/camera.mjs
@@ -2,11 +2,36 @@ import {addPanelEvents} from "../libs/etc/events.mjs"
 
 export function addCameraPanel() {
 
-  $("body").append("<div id='camera' class='panel'><h3>Camera</h3></div>")
+  $("body").append("<div id='camera' class='panel'></div>")
 
   let panel = $("#camera.panel")
 
   panel.append("<img class='close' src='/app/imgs/panels/close.png'>")
+
+  panel.append("<img class='camera' src='/app/imgs/panels/camera.png'>")
+
+  panel.append("<h4>Drag Speed</h4>")
+  panel.append("<input type='range' id='drag' class='range' min='1' max='100' value='50'>")
+  panel.append("<h4>Fly Speed</h4>")
+  panel.append("<input type='range' id='fly' class='range' min='1' max='100' value='50'>")
+  panel.append("<h4>Spin Speed</h4>")
+  panel.append("<input type='range' id='spin' class='range' min='1' max='100' value='50'>")
+
+  $(".range").mousedown(function(event) {
+
+    event.stopPropagation()
+
+    $(this).css("cursor", "url('app/imgs/icons/cursors/grabbing.png'), grabbing")
+
+  })
+
+  $(".range").mouseup(function(event) {
+
+    event.stopPropagation()
+
+    $(this).css("cursor", "url('app/imgs/icons/cursors/grab.png'), grab")
+
+  })
 
   addPanelEvents(panel)
 
