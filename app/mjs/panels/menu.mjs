d119dcf3272e97d5bbc10b246525b8c58e10bb49
Jacob Philpott <jacob.philpott@gmx.com>
2022-03-04 12:56:58 +0300

    menu panel update

--- app/mjs/panels/menu.mjs
+++ app/mjs/panels/menu.mjs
@@ -2,31 +2,111 @@ import {addPanelEvents} from "../libs/etc/events.mjs"
 
 export function addMenuPanel() {
 
-  $("body").append("<div id='menu' class='panel'></div>")
+  $("body").append("<div id='menu' class='panel'><img class='close' src='/app/imgs/panels/nav/close.png'></div>")
 
   let panel = $("#menu.panel")
 
-  panel.append("<img class='close' src='/app/imgs/panels/nav/close.png'>")
-
-  panel.append("<div class='head'><img id='logo' src='/app/imgs/theme/logo.png'><h2 id='name'>Polymaker</h2></div>")
-
-  panel.append("<div id='export-import' class='option'><h4>Export / Import</h4></div>")
-  panel.append("<div id='panels' class='option'><h4>Panels</h4></div>")
-  panel.append("<div id='settings' class='option'><h4>Settings</h4></div>")
-
   var client = false
 
-  if (client) {
+  function appendMain() {
 
-    panel.append("<div id='profile' class='option opt'><h4>Profile</h4></div>")
-    panel.append("<div id='logout' class='option opt'><h4>Logout</h4></div>")
+    let main = "<div id='main' class='sub-panel'>"
 
-  } else {
+    main += "<div class='head'><img id='logo' src='/app/imgs/theme/logo.png'><h2 id='title'>Polymaker</h2></div>"
 
-    panel.append("<div id='signup' class='option opt'><h4>Sign Up</h4></div>")
-    panel.append("<div id='login' class='option opt'><h4>Login</h4></div>")
+    main += "<div id='export-import' class='option'><h4>Export / Import</h4></div>"
+    main += "<div id='panels' class='option'><h4>Panels</h4></div>"
+    main += "<div id='settings' class='option'><h4>Settings</h4></div>"
+
+    if (client) {
+
+      main += "<div id='profile' class='option opt'><h4>Profile</h4></div>"
+      main += "<div id='logout' class='option opt'><h4>Logout</h4></div>"
+
+    } else {
+
+      main += "<div id='signup' class='option opt'><h4>Sign Up</h4></div>"
+      main += "<div id='login' class='option opt'><h4>Login</h4></div>"
+
+    }
+
+    panel.append(main + "</div>")
 
   }
+
+  function appendExportImport() {
+
+    let porting = "<div id='export-import-panel' class='sub-panel'><h3 id='title'>Export / Import</h3>"
+
+    panel.append(porting + "</div>")
+
+  }
+
+  function appendPanels() {
+
+    let panels = "<div id='panels-panel' class='sub-panel'><h3 id='title'>Panels</h3>"
+
+    panel.append(panels + "</div>")
+
+  }
+
+  function appendSettings() {
+
+    let settings = "<div id='settings-panel' class='sub-panel'><h3 id='title'>Settings</h3>"
+
+    panel.append(settings + "</div>")
+
+  }
+
+  function appendSignup() {
+
+    let signup = "<div id='panels-panel' class='sub-panel'><h3 id='title'>Signup</h3>"
+
+    panel.append(signup + "</div>")
+
+  }
+
+  function appendProfile() {
+
+    let profile = "<div id='panels-panel' class='sub-panel'><h3 id='title'>Profile</h3>"
+
+    panel.append(profile + "</div>")
+
+  }
+
+  function appendLogin() {
+
+    let login = "<div id='panels-panel' class='sub-panel'><h3 id='title'>Login</h3>"
+
+    panel.append(login + "</div>")
+
+  }
+
+  function appendLogout() {
+
+    let logout = "<div id='panels-panel' class='sub-panel'><h3 id='title'>Logout</h3>"
+
+    panel.append(logout + "</div>")
+
+  }
+
+  appendMain()
+
+  appendExportImport()
+  appendPanels()
+  appendSettings()
+
+  if (client) {
+    appendProfile()
+    appendLogout()
+  } else {
+    appendSignup()
+    appendLogin()
+  }
+
+  $("#menu.panel .option").click(function(event) {
+    console.log(this.id)
+  })
 
   $("#nav #menu").click(function() { toggleMenu() })
 
