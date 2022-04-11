5f37ace5c2e1e6220bb8fee4e46db8e2ebc7e669
Jacob Philpott <jacob.philpott@gmx.com>
2022-04-11 21:36:40 +0300

    renaming in progress

--- app/mjs/panels/all.mjs
+++ app/mjs/panels/all.mjs
@@ -1,17 +1,17 @@
 import {addCameraPanel} from "./camera.mjs"
 import {addContextPanel} from "./context.mjs"
-import {addGeometriesPanel} from "./geometries.mjs"
 import {addMenuPanel} from "./menu.mjs"
 import {addMeshesPanel} from "./meshes.mjs"
 import {addShapesPanel} from "./shapes.mjs"
+import {addShortcutPanel} from "./shortcut.mjs"
 
 export function addPanels() {
 
   addCameraPanel()
-  addGeometriesPanel()
   addMenuPanel()
   addMeshesPanel()
   addShapesPanel()
+  addShortcutPanel()
 
   addContextPanel()
 
