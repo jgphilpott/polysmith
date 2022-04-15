be8105b2cd76efb09f513d9a1125a561c4b0b044
Jacob Philpott <jacob.philpott@gmx.com>
2022-04-16 01:22:37 +0300

    line geometry update

--- app/mjs/libs/geometries/lines.mjs
+++ app/mjs/libs/geometries/lines.mjs
@@ -4,23 +4,23 @@ import {black} from "../colors/three/grayscale.mjs"
 import {addMesh} from "../../panels/mesh.mjs"
 import {lineMaterial} from "../materials/line.mjs"
 
-export function newVector(vertices=[[]]) {
+export function lineGeometry(vertices=[[10, 10, 10], [-10, -10, -10]]) {
 
-  let vector = new THREE.Geometry()
+  let geometry = new THREE.Geometry()
 
   for (let i = 0; i < vertices.length; i++) {
 
-    vector.vertices.push(new THREE.Vector3(vertices[i][0], vertices[i][1], vertices[i][2]))
+    geometry.vertices.push(new THREE.Vector3(vertices[i][0], vertices[i][1], vertices[i][2]))
 
   }
 
-  return vector
+  return geometry
 
 }
 
 export function newLine(vertices=[[10, 10, 10], [-10, -10, -10]], material="basic", color=black, linewidth=1, dashed=false, dashSize=5, gapSize=3) {
 
-  let line = new THREE.Line(newVector(vertices), lineMaterial(material, color, linewidth, dashed, dashSize, gapSize)).computeLineDistances()
+  let line = new THREE.Line(lineGeometry(vertices), lineMaterial(material, color, linewidth, dashed, dashSize, gapSize)).computeLineDistances()
 
   line.class = "line"
 
---
+++ app/mjs/libs/geometries/lines2.mjs
@@ -0,0 +1,36 @@
+import {LineGeometry} from "../../../libs/js/LineGeometry.mjs"
+import {lineMaterial} from "../materials/line.mjs"
+import {LineMesh} from "../../../libs/js/LineMesh.mjs"
+
+import {black} from "../colors/solid/grayscale.mjs"
+import {addMesh} from "../../panels/mesh.mjs"
+
+export function lineGeometry(vertices=[[10, 10, 10], [-10, -10, -10]]) {
+
+  let positions = []
+
+  for (let i = 0; i < vertices.length; i++) {
+
+    positions.push(vertices[i][0], vertices[i][1], vertices[i][2])
+
+  }
+
+  return new LineGeometry().setPositions(positions)
+
+}
+
+export function newLine2(vertices=[[10, 10, 10], [-10, -10, -10]], material="fat", color=black, linewidth=1, dashed=false, dashSize=5, gapSize=3) {
+
+  let line = new LineMesh(lineGeometry(vertices), lineMaterial(material, color, linewidth, dashed, dashSize, gapSize)).computeLineDistances()
+
+  line.class = "line"
+
+  return line
+
+}
+
+export function addLine2(vertices=[[10, 10, 10], [-10, -10, -10]], material="fat", color=black, linewidth=1, dashed=false, dashSize=5, gapSize=3) {
+
+  return addMesh(newLine2(vertices, material, color, linewidth, dashed, dashSize, gapSize))
+
+}
