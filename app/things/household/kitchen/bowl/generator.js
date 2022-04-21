da088d7cd2740d270a501e86eede472952d18559
Jacob Philpott <jacob.philpott@gmx.com>
2022-04-21 20:59:05 +0300

    starting kitchen bowl

---
+++ app/things/household/kitchen/bowl/generator.js
@@ -0,0 +1,34 @@
+function generator(type="classic") {
+
+  let radius = 75
+  let thickness = 2
+  var bowlSegments
+
+  if (type == "classic") {
+    bowlSegments = 50
+  } else if (type == "polygon") {
+    bowlSegments = 10
+  }
+
+  let bowl = newSphere(radius, bowlSegments, bowlSegments)
+  let bowlCutout = newSphere(radius - thickness, bowlSegments, bowlSegments)
+  let boxCutout = newBox(radius * 2, radius * 2, radius, [0, 0, -(radius * 1.333)])
+  let lip = newTorus(radius - (thickness / 2), thickness, bowlSegments / 2, bowlSegments)
+
+  lip.rotation.y = degree2radian(0)
+  bowl.rotation.x = degree2radian(90)
+  bowlCutout.rotation.x = degree2radian(90)
+
+  bowl = cutMesh(bowl, boxCutout)
+  boxCutout.position.z += thickness
+  bowlCutout = cutMesh(bowlCutout, boxCutout)
+
+  boxCutout.position.z = radius / 2
+  bowl = cutMesh(bowl, boxCutout)
+
+  bowl = cutMesh(bowl, bowlCutout)
+
+  scene.add(bowl)
+  scene.add(lip)
+
+}
