8bc822124b20c1ce0b1f6342ae040093e3e8ea8f
Jacob Philpott <jacob.philpott@gmx.com>
2022-04-23 16:19:44 +0300

    camera panel update

--- app/mjs/panels/camera.mjs
+++ app/mjs/panels/camera.mjs
@@ -7,40 +7,42 @@ export function addCameraPanel() {
   let panel = $("#camera.panel")
 
   panel.append("<img class='camera' src='/app/imgs/panels/main/camera.png'>")
+  panel.append("<img class='refresh' src='/app/imgs/panels/tools/refresh.png'>")
 
   panel.append("<div id='position' class='controls'><div class='head'><img class='fold' src='/app/imgs/panels/nav/fold.png'><h4>Position</h4></div><div class='body'></div></div>")
   panel.append("<div id='target' class='controls'><div class='head'><img class='fold' src='/app/imgs/panels/nav/fold.png'><h4>Target</h4></div><div class='body'></div></div>")
   panel.append("<div id='speed' class='controls'><div class='head'><img class='fold' src='/app/imgs/panels/nav/fold.png'><h4>Speed</h4></div><div class='body'></div></div>")
 
-  let position = $("#camera.panel #position .body")
-  let target = $("#camera.panel #target .body")
-  let speed = $("#camera.panel #speed .body")
+  panel.find(".fold, h4").click(function(event) { fold(this) })
 
-  let dragSpeed = settings.controls.dragSpeed
-  let flySpeed = settings.controls.flySpeed
-  let spinSpeed = settings.controls.spinSpeed
+  let position = panel.find("#position .body")
+  let target = panel.find("#target .body")
+  let speed = panel.find("#speed .body")
 
-  position.append("<span id='position-x'><label id='x'>X</label> <input type=number step=1 min=" + -(scale * 5) + " max=" + scale * 5 + "><button id='plus'>+</button><button id='minus'>-</button></span>")
-  position.append("<span id='position-y'><label id='y'>Y</label> <input type=number step=1 min=" + -(scale * 5) + " max=" + scale * 5 + "><button id='plus'>+</button><button id='minus'>-</button></span>")
-  position.append("<span id='position-z'><label id='z'>Z</label> <input type=number step=1 min=" + -(scale * 5) + " max=" + scale * 5 + "><button id='plus'>+</button><button id='minus'>-</button></span>")
+  let min = -scale * 5
+  let max = scale * 5
 
-  target.append("<span id='position-x'><label id='x'>X</label> <input type=number step=1 min=" + -(scale * 5) + " max=" + scale * 5 + "><button id='plus'>+</button><button id='minus'>-</button></span>")
-  target.append("<span id='position-y'><label id='y'>Y</label> <input type=number step=1 min=" + -(scale * 5) + " max=" + scale * 5 + "><button id='plus'>+</button><button id='minus'>-</button></span>")
-  target.append("<span id='position-z'><label id='z'>Z</label> <input type=number step=1 min=" + -(scale * 5) + " max=" + scale * 5 + "><button id='plus'>+</button><button id='minus'>-</button></span>")
+  position.append("<span id='position-x'><label id='x'>X</label><input type=number step=1 min=" + min + " max=" + max + " value=" + camera.position.x.toFixed(2) + "><button id='plus'>+</button><button id='minus'>-</button></span>")
+  position.append("<span id='position-y'><label id='y'>Y</label><input type=number step=1 min=" + min + " max=" + max + " value=" + camera.position.y.toFixed(2) + "><button id='plus'>+</button><button id='minus'>-</button></span>")
+  position.append("<span id='position-z'><label id='z'>Z</label><input type=number step=1 min=" + min + " max=" + max + " value=" + camera.position.z.toFixed(2) + "><button id='plus'>+</button><button id='minus'>-</button></span>")
+
+  target.append("<span id='target-x'><label id='x'>X</label><input type=number step=1 min=" + min + " max=" + max + " value=" + camera.target.x.toFixed(2) + "><button id='plus'>+</button><button id='minus'>-</button></span>")
+  target.append("<span id='target-y'><label id='y'>Y</label><input type=number step=1 min=" + min + " max=" + max + " value=" + camera.target.y.toFixed(2) + "><button id='plus'>+</button><button id='minus'>-</button></span>")
+  target.append("<span id='target-z'><label id='z'>Z</label><input type=number step=1 min=" + min + " max=" + max + " value=" + camera.target.z.toFixed(2) + "><button id='plus'>+</button><button id='minus'>-</button></span>")
 
   speed.append("<p>Drag Speed</p><div id='drag' class='slider'></div>")
   speed.append("<p>Fly Speed</p><div id='fly' class='slider'></div>")
-  speed.append("<p>Spin Speed</p><div id='spin' class='slider'></div>")
+  speed.append("<p>Zoom Speed</p><div id='zoom' class='slider'></div>")
 
-  $("#drag.slider").slider({min: 1, max: dragSpeed * 3, value: dragSpeed, start: sliderStart, slide: sliderSlide, stop: sliderStop})
-  $("#fly.slider").slider({min: 1, max: flySpeed * 3, value: flySpeed, start: sliderStart, slide: sliderSlide, stop: sliderStop})
-  $("#spin.slider").slider({min: 1, max: spinSpeed * 3, value: spinSpeed, start: sliderStart, slide: sliderSlide, stop: sliderStop})
+  speed.find("#drag.slider").slider({min: 1, max: 100, value: settings.controls.dragSpeed, start: sliderStart, slide: sliderSlide, stop: sliderStop})
+  speed.find("#fly.slider").slider({min: 1, max: 100, value: settings.controls.flySpeed, start: sliderStart, slide: sliderSlide, stop: sliderStop})
+  speed.find("#zoom.slider").slider({min: 1, max: 100, value: settings.controls.zoomSpeed, start: sliderStart, slide: sliderSlide, stop: sliderStop})
 
-  $("#camera.panel .fold, #camera.panel h4").click(function(event) { fold(this) })
+  sliderStyle(speed.find("#drag.slider"))
+  sliderStyle(speed.find("#fly.slider"))
+  sliderStyle(speed.find("#zoom.slider"))
 
-  rotate($("#camera.panel #speed .fold"), 90, 0)
-
-  sliderStyle($("#camera.panel .slider"))
+  rotate(panel.find("#speed .fold"), 90, 0)
 
   addPanelEvents(panel)
 
