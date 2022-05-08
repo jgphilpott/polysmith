25c6da73d16980c7a082ea79aa81c073921242f6
Jacob Philpott <jacob.philpott@gmx.com>
2022-05-08 10:15:06 +0300

    tools update

--- app/mjs/libs/etc/tools.js
+++ app/mjs/libs/etc/tools.js
@@ -1,7 +1,9 @@
+// Credit: https://stackoverflow.com/a/64777515/1544937
+const chunkArray = (a, n) => [...Array(Math.ceil(a.length / n))].map((_, i) => a.slice(n * i, n + n * i))
+
 function calculatePercent(min, max, value) { return (value - min) / (max - min) * 100 }
 
-// Credit: https://stackoverflow.com/a/64777515/1544937
-const chunkArray = (a,n)=>[...Array(Math.ceil(a.length/n))].map((_,i)=>a.slice(n*i,n+n*i))
+function signedVolumeOfTriangle(p1, p2, p3) { return p1.dot(p2.cross(p3)) / 6.0 }
 
 // Credit: https://stackoverflow.com/a/5624139/1544937
 function component2hex(component) {
@@ -31,18 +33,18 @@ function hex2rgb(hex) {
 // Credit: http://gist.github.com/399624
 // The click event will be fired with a small delay but will not fire upon a double click.
 $.fn.clickSingleDouble = function(single_click_callback, double_click_callback, delay=333) {
-  return this.each(function(){
-    var clicks = 0, self = this
-    jQuery(this).click(function(event){
+  return this.each(function() {
+    let clicks = 0, self = this
+    jQuery(this).click(function(event) {
       clicks++
       if (clicks == 1) {
-        setTimeout(function(){
-          if(clicks == 1) {
+        setTimeout(function() {
+          if (clicks == 1) {
             single_click_callback.call(self, event)
           } else {
             double_click_callback.call(self, event)
           }
-          clicks = 0;
+          clicks = 0
         }, delay)
       }
     })
@@ -72,9 +74,26 @@ Array.prototype.filterInPlace = function(condition, item) {
 
 }
 
-function signedVolumeOfTriangle(p1, p2, p3) {
-  return p1.dot(p2.cross(p3)) / 6.0
+function clearMeshOperation() {
+
+  let operationIcons = $("#mesh.panel img.operation")
+
+  operationIcons.toArray().forEach(icon => {
+
+    if (!$(icon).hasClass("disabled")) $(icon).attr("src", "/app/imgs/panels/ops/default/" + icon.id + ".png")
+
+  })
+
+  $("#canvas").css("cursor", "")
+
+  events.operation.mesh = null
+  events.operation.key = null
+
 }
+
+function centerX(boundingBox) { return - boundingBox.min.x - (Math.abs(boundingBox.max.x - boundingBox.min.x) / 2) }
+function centerY(boundingBox) { return - boundingBox.min.y - (Math.abs(boundingBox.max.y - boundingBox.min.y) / 2) }
+function centerZ(boundingBox) { return - boundingBox.min.z - (Math.abs(boundingBox.max.z - boundingBox.min.z) / 2) }
 
 function getBoundingBox(mesh) {
 
@@ -105,18 +124,6 @@ function getBoundingBox(mesh) {
 
   }
 
-}
-
-function centerX(boundingBox) {
-  return - boundingBox.min.x - (Math.abs(boundingBox.max.x - boundingBox.min.x) / 2)
-}
-
-function centerY(boundingBox) {
-  return - boundingBox.min.y - (Math.abs(boundingBox.max.y - boundingBox.min.y) / 2)
-}
-
-function centerZ(boundingBox) {
-  return - boundingBox.min.z - (Math.abs(boundingBox.max.z - boundingBox.min.z) / 2)
 }
 
 // Credit: https://stackoverflow.com/a/13091694/1544937
