c9d3b49107f6ae2fdd747d750ce40e8e8df367d6
Jacob Philpott <jacob.philpott@gmx.com>
2022-05-27 14:42:08 +0300

    camera update

--- app/mjs/libs/cameras/perspective.mjs
+++ app/mjs/libs/cameras/perspective.mjs
@@ -3,14 +3,14 @@ export function addPerspectiveCamera() {
 
   let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, scale / 1000, scale * 10)
 
+  camera.up.set(0, 0, 1)
+
   let position = settings.camera.position
   let target = settings.camera.target
 
-  camera.up.set(0, 0, 1)
-  camera.target = target
-
   camera.position.set(position.x, position.y, position.z)
   camera.lookAt(target.x, target.y, target.z)
+  camera.target = target
 
   return camera
 
