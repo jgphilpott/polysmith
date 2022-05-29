023851655c8f0f570fa0da83e9f38475379f1502
Jacob Philpott <jacob.philpott@gmx.com>
2022-05-29 11:56:58 +0300

    drag controls update

--- app/mjs/libs/controls/drag.mjs
+++ app/mjs/libs/controls/drag.mjs
@@ -1,40 +1,39 @@
 export function addDragControls() {
 
-  let radius3 = null
-  let startX, startY = null
+  const satoshi = 0.000001
+  const panel = $("#camera.panel")
+
+  let radius3, startX, startY = null
   let target, position, dragSpeed = null
   let horizontalAngle, verticalAngle = null
 
   function start(event) {
 
-    event.preventDefault()
-    event.stopPropagation()
-
     camera.dragged = null
 
-    target = camera.target
-    position = camera.position
-
-    dragSpeed = settings.controls.dragSpeed
+    event.preventDefault()
+    event.stopPropagation()
 
     startX = event.pageX
     startY = event.pageY
 
+    target = camera.target
+    position = camera.position
+
+    dragSpeed = settings.controls.dragSpeed / 500
+
     let deltaX = Math.abs(position.x - target.x)
-    let deltaY = Math.abs(position.y - target.y)
     let deltaZ = Math.abs(position.z - target.z)
 
     radius3 = position.distanceTo(target)
     let radius2 = side4sides(deltaZ, null, radius3)
 
     horizontalAngle = angle4sides(radius2, deltaX)
-    if (position.x < target.x) { horizontalAngle = 180 - horizontalAngle }
-    if (position.y < target.y) { horizontalAngle = -horizontalAngle }
+    if (position.x < target.x) horizontalAngle = 180 - horizontalAngle
+    if (position.y < target.y) horizontalAngle = - horizontalAngle
 
     verticalAngle = angle4sides(radius3, deltaZ)
-    if (position.z < target.z) { verticalAngle = 180 - verticalAngle }
-
-    dragSpeed = settings.controls.dragSpeed / 500
+    if (position.z < target.z) verticalAngle = 180 - verticalAngle
 
     document.onmousemove = drag
     document.onmouseup = stop
@@ -43,26 +42,26 @@ export function addDragControls() {
 
   function drag(event) {
 
+    camera.dragged = true
+
     event.preventDefault()
     event.stopPropagation()
-
-    camera.dragged = true
 
     let radius2 = side4sides(Math.abs(position.z - target.z), null, radius3)
 
     let newHorizontalAngle = horizontalAngle + ((startX - event.pageX) * dragSpeed)
-    if (newHorizontalAngle >= 180) { newHorizontalAngle = newHorizontalAngle - 360 }
+    if (newHorizontalAngle >= 180) newHorizontalAngle = newHorizontalAngle - 360
 
     let newVerticalAngle = verticalAngle + ((startY - event.pageY) * dragSpeed)
-    if (newVerticalAngle <= 0) { newVerticalAngle = 0.000001 } else if (newVerticalAngle >= 180) { newVerticalAngle = -180 - 0.000001 }
+    if (newVerticalAngle <= 0) { newVerticalAngle = satoshi } else if (newVerticalAngle >= 180) { newVerticalAngle = - 180 - satoshi }
 
     let newX = side4angle(newHorizontalAngle, radius2, true, null) + target.x
     let newY = side4angle(newHorizontalAngle, radius2, null, true) + target.y
     let newZ = side4angle(newVerticalAngle, radius3, true, null) + target.z
 
-    $("#camera.panel #position-x input").val(newX.toFixed(2))
-    $("#camera.panel #position-y input").val(newY.toFixed(2))
-    $("#camera.panel #position-z input").val(newZ.toFixed(2))
+    panel.find("#position-x input").val(newX.toFixed(2))
+    panel.find("#position-y input").val(newY.toFixed(2))
+    panel.find("#position-z input").val(newZ.toFixed(2))
 
     position.set(newX, newY, newZ)
     camera.lookAt(target.x, target.y, target.z)
