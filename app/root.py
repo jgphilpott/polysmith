47213a0e576c2c6bfbfaf8509ae1439ff11366ac
Jacob Philpott <jacob.philpott@gmx.com>
2022-08-17 14:49:20 +0300

    root update

--- app/root.py
+++ app/root.py
@@ -98,7 +98,7 @@ if not exists(libs_dir):
 
     with open(js_libs_dir + "/tools.js", "w") as file:
 
-        camalize = get("https://gist.githubusercontent.com/jgphilpott/19e7a94cdf6d6d4cd868cc18e628026c/raw/2c5d68bb84f0d4e14478bcac359a77137f6a25f5/camalize.js").content.decode("utf-8")
+        casefy = get("https://gist.githubusercontent.com/jgphilpott/19e7a94cdf6d6d4cd868cc18e628026c/raw/6a34b4074cc8871eb882dd0bc32ca12eff1b4aa4/casefy.js").content.decode("utf-8")
         rotation = get("https://gist.githubusercontent.com/jgphilpott/1bc17b82063f14fabb8f3e38825f6f10/raw/b5ddf5f386213f47ac4fd4b9f41bc116b37f29a3/rotation.js").content.decode("utf-8")
         distance = get("https://gist.githubusercontent.com/jgphilpott/da9da7d8bdfb32982f99d9910efb1410/raw/51b50b26fc144e3cd2e2849ad47b280d5d1152f5/distanceConversion.js").content.decode("utf-8")
         cookieFuncs = get("https://gist.githubusercontent.com/jgphilpott/b9ce64b9ef8b04c5ac58902b133b1a28/raw/8931a5cd26c48945e932a7399f853b593687f557/cookieFunctions.js").content.decode("utf-8")
@@ -107,7 +107,7 @@ if not exists(libs_dir):
         format = get("https://gist.githubusercontent.com/jgphilpott/787659ac4ea57a9971da58a76191079b/raw/d87c450947083ab134999408cec38fb70756593a/numberFormater.js").content.decode("utf-8")
         subset = get("https://gist.githubusercontent.com/jgphilpott/a1367ca419ac2807ed4340d69356b7f1/raw/48ad3970a6a370853d18157142421ab02b1e2398/subset.js").content.decode("utf-8")
 
-        tools = camalize + "\n" + rotation + "\n" + distance + "\n" + cookieFuncs + "\n" + localStorage + "\n" + validEmail + "\n" + format + "\n" + subset
+        tools = casefy + "\n" + rotation + "\n" + distance + "\n" + cookieFuncs + "\n" + localStorage + "\n" + validEmail + "\n" + format + "\n" + subset
 
         file.write(tools)
 
